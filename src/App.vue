5f68e189234999caa91cdbdb0cb4796df248245b
Markson Rebelo Marcolino <markson.marcolino@gmail.com>
2026-04-13 19:29:31 +0200

    fix: remove mousemove event listener leak in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import { useMediaQuery } from '@vueuse/core'
 import gsap from 'gsap'
 import NavBar from './components/NavBar.vue'
@@ -7,18 +7,24 @@ import NavBar from './components/NavBar.vue'
 const cursorGlow = ref(null)
 const hasHover = useMediaQuery('(hover: hover)')
 
+function onMouseMove(e) {
+  if (cursorGlow.value) {
+    gsap.to(cursorGlow.value, {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 0.8,
+      ease: 'power3.out',
+    })
+  }
+}
+
 onMounted(() => {
   if (!hasHover.value) return
-  window.addEventListener('mousemove', (e) => {
-    if (cursorGlow.value) {
-      gsap.to(cursorGlow.value, {
-        x: e.clientX,
-        y: e.clientY,
-        duration: 0.8,
-        ease: 'power3.out',
-      })
-    }
-  })
+  window.addEventListener('mousemove', onMouseMove)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('mousemove', onMouseMove)
 })
 </script>
 
